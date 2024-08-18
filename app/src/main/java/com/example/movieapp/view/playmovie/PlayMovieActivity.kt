6dbe6b8ee44ffc8f8794b7cc94b1527f2f7649ca
dangh <danghiep101@gmail.com>
2024-08-18 17:15:43 +0700

    Update logo

--- app/src/main/java/com/example/movieapp/view/playmovie/PlayMovieActivity.kt
+++ app/src/main/java/com/example/movieapp/view/playmovie/PlayMovieActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.viewModels
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.hls.HlsMediaSource
@@ -15,8 +16,8 @@ import com.example.movieapp.databinding.ActivityPlayMovieBinding
 import com.example.movieapp.viewmodel.PlayMovieViewModel
 
 class PlayMovieActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPlayMovieBinding
-    private lateinit var player: ExoPlayer
+    private var binding: ActivityPlayMovieBinding? = null
+    private var player: ExoPlayer? = null
     private val viewModel: PlayMovieViewModel by viewModels()
 
     private var currentPosition: Long = 0L
@@ -26,79 +27,66 @@ class PlayMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayMovieBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
         val linkPlayMovie = intent.getStringExtra("LINK_PLAY_MOVIE")
 
         if (linkPlayMovie != null) {
             viewModel.setMovieData(linkPlayMovie)
+            setupPlayer(linkPlayMovie)
         }
-
         viewModel.linkPlayMovie.observe(this) { link ->
-            if (!::player.isInitialized) {
-                setupPlayer(link)
-            } else {
-                player.setMediaSource(
-                    HlsMediaSource.Factory(DefaultHttpDataSource.Factory())
-                        .createMediaSource(MediaItem.fromUri(link))
-                )
-                player.prepare()
-                player.playWhenReady = isPlaying
-            }
-            player.seekTo(currentPosition)
+            player?.setMediaSource(
+                HlsMediaSource.Factory(DefaultHttpDataSource.Factory())
+                    .createMediaSource(MediaItem.fromUri(link))
+            )
+            player?.prepare()
+            player?.playWhenReady = isPlaying
         }
-
 
         if (savedInstanceState != null) {
             currentPosition = savedInstanceState.getLong("CURRENT_POSITION", 0L)
             isPlaying = savedInstanceState.getBoolean("IS_PLAYING", true)
+            player?.prepare()
+            player?.playWhenReady = true
+            player?.seekTo(currentPosition)
         }
         hideSystemUI()
     }
 
+
     @OptIn(UnstableApi::class)
     private fun setupPlayer(linkPlayMovie: String) {
         player = ExoPlayer.Builder(this).build()
-        binding.playerView.player = player
+        binding?.playerView?.player = player
         val dataSourceFactory = DefaultHttpDataSource.Factory()
-        val mediaSource: MediaSource = HlsMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(MediaItem.fromUri(linkPlayMovie))
-
-        player.setMediaSource(mediaSource)
-        player.prepare()
-        player.playWhenReady = isPlaying
-        player.seekTo(currentPosition)
+        player?.seekTo(currentPosition)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        if (::player.isInitialized) {
-            outState.putLong("CURRENT_POSITION", player.currentPosition)
-            outState.putBoolean("IS_PLAYING", player.playWhenReady)
-        }
+        outState.putLong("CURRENT_POSITION", player?.currentPosition!!)
+        outState.putBoolean("IS_PLAYING", player?.playWhenReady!!)
+
     }
 
     override fun onStart() {
         super.onStart()
-        if (::player.isInitialized) {
-            player.playWhenReady = isPlaying
-        }
+        player?.playWhenReady = isPlaying
+
     }
 
     override fun onStop() {
         super.onStop()
-        if (::player.isInitialized) {
-            isPlaying = player.playWhenReady
-            player.playWhenReady = false
-        }
+        isPlaying = player?.playWhenReady!!
+        player?.playWhenReady = false
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (::player.isInitialized) {
-            player.release()
-        }
     }
+
     private fun hideSystemUI() {
 
         window.decorView.systemUiVisibility = (
@@ -108,3 +96,4 @@ class PlayMovieActivity : AppCompatActivity() {
                 )
     }
 }
+
